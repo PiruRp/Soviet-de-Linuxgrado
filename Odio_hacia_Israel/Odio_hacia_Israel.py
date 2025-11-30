7fa2e5a63b0d62e744c63d6a806b6cae60732233
PiruRp <piruyeray@gmail.com>
2025-11-30 16:45:37 +0100

    Add files via upload

--- Odio_hacia_Israel/Odio_hacia_Israel.py
+++ Odio_hacia_Israel/Odio_hacia_Israel.py
@@ -10,6 +10,6 @@ def _promedio():
         números = [int(linea.strip()) for linea in líneas] #"strip" sirve para eliminar espacios en blanco y saltos de línea al principio y al final de la cadena. "for linea in líneas" dice que tiene que llaamr a eso "linea" y hacerlo en cada valor de "línea".
         promedio = sum(números) / len(números) #len(números) = cantidad de dígitos en "números"
     print("De media, tu odio hacia el ilegítimo estado genocida de Israel es", promedio)
-    if promedio == 10:
+    if promedio >= 10:
         print("Muy bien")
 _promedio()
